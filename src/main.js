f8b642e07c9e8db7d1f3a571387f53527f5d80cc
matemartiin <matemartiin@gmail.com>
2025-07-05 17:10:44 -0300

    Deleted main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-@@ .. @@
- import { ReportManager } from './modules/reports.js';
- import { ThemeManager } from './modules/theme.js';
- import { NavigationManager } from './modules/navigation.js';
-
- class FinznApp {
