5d89cec4d691b8abdd4ada641d60a23e9b90815f
matemartiin <matemartiin@gmail.com>
2025-07-05 17:10:41 -0300

    Sistema de Límites de Gastos con Alertas de Mascota

---
+++ src/main.js
@@ -0,0 +1,6 @@
+@@ .. @@
+ import { ReportManager } from './modules/reports.js';
+ import { ThemeManager } from './modules/theme.js';
+ import { NavigationManager } from './modules/navigation.js';
+
+ class FinznApp {
