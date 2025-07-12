a46461f508515cf662d5428691d96a7d32ed2c20
matemartiin <matemartiin@gmail.com>
2025-07-12 17:58:27 -0300

    Fix Mixed Content Error - Align Frontend to HTTP

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+@@ .. @@
+ import { defineConfig } from 'vite'
+
+ export default defineConfig({
+   root: '.',
+   build: {
+     outDir: 'dist'
+   },
+   server: {
+-    port: 3000
++    port: 3000,
++    https: false
+   }
+ })
